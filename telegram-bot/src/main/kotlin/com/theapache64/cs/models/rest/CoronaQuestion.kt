623b628df23f7d6e5df503552aa83a4f65c3d6ac
sayedah <59183578+sayedah@users.noreply.github.com>
2021-03-14 19:39:04 -0400

    Create CoronaQuestion.kt
    Created a class for a Question object which compartmentalizes all the fields that a question will require through its lifespan. References translator methods, scrapers, nlp's. When ready to display all the information desired for the user,, all the data for the question is in one unit and ready to be extracted.

--- telegram-bot/src/main/kotlin/com/theapache64/cs/models/rest/CoronaQuestion.kt
+++ telegram-bot/src/main/kotlin/com/theapache64/cs/models/rest/CoronaQuestion.kt
@@ -8,3 +8,31 @@ class CoronaQuestion(
     @SerializedName("top_k_retriever")
     val resultCount: Int = 1
 )
+
+
+//Aggregate Pattern
+Class Question:
+	def init(self, QuestID: str, question: str language):
+		self.QuestID = QuestID
+		self.quest = question
+       	self.lang = language
+        self.source = null       //question initially has no answer and source of information for the answer
+        self.ans = null 
+		self.embedding = get.embedding()   //store embeddings of question when it's time to match user's input 
+
+        def answer(self, source : DataSource, answer : str) 
+		//after scrapers/ElasticSearch are used, assign 'answer' to Question along with the 'source' the information was pulled from
+                self.source = DataSource
+                self.ans = answer
+
+        def translate(self, QuestID, language):
+                if self.lang != language 
+                self.lang = language  //call on translators to translate to parameterized language 
+
+        def match(self) 
+                
+
+        
+
+        
+
