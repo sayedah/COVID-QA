d88ce269cd3a85080016290741fc9a1e5670def2
sayedah <59183578+sayedah@users.noreply.github.com>
2021-04-15 13:09:15 -0400

    Update deduplicate_with_sentenceBert.py to include Creational Pattern
    Applied a singleton pattern to the database of questions/answers, treating it as a single globally accessible instance. Acting as a cache, the set of questions and answers can be accessed through one instance by user and developers alike throughout the system

--- data/faqs/deduplicate_with_sentenceBert.py
+++ data/faqs/deduplicate_with_sentenceBert.py
@@ -2,6 +2,11 @@ from haystack.retriever.elasticsearch import ElasticsearchRetriever
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+import covid_nlp.language.detect_language as detect
+import covid_nlp.language.ms_translate as translate
+import covid_nlp.eval
+import covid_nlp.modeling.tfidf, covid_nlp.modeling.transformer
+
 
 # loading questions and calculating similarities based of sentence bert embeddings
 df = pd.read_csv("200416_englishFAQ.csv",sep=",")
@@ -40,5 +45,40 @@ newdf = df.iloc[indices,:]
 print(newdf.shape)
 print(df.shape)
 newdf.to_csv("200416_CDCGen_dedup.csv",index=True,sep=",")
+
+
+##Creational Pattern
+Class QuestionSet ():
+  static private QuestionSet instance
+
+  def private _init_()
+    
+  def static QuestionSet getInstance():
+    if (instance == null):
+        instance = new QuestionSet()
+
+  def getQuestions():
+    ret = String []
+    dir = 'COVID-QA/data/faqs'
+    with open(dir, 'faq_covidbert.csv') as csvFile:
+        text = csv.reader(csvFile, delimiter=',')
+        for row in text:
+            return (row[1], row[2])                                            #questions are in the second column of the csv file
+
+  def query(self, S):
+    questionLang = detect.detect_lang(self, S)                                 #detect language using method imported from covidnlp/lang/detectlanguage/import
+    ques = translate.MSTranslator(self, None, None, "en").translate(self, S)   #translate input question to English so it can be detected in the .csv file
+    dir = 'COVID-QA/data/faqs/'                                                #obtain directory for .csv file containing questions and answers
+    with open(dir, 'faq_covidbert.csv') as csvFile:                            #open file
+        #search for corresponding answer to question
+        text = csv.reader(csvFile, delimiter=",")                              #questions were separated from the answers using commas
+        for row in text:                                                       #search the file for the question
+                if ques in row:                                                #question was found
+                    ans = row[2]                                               #answers were in the 3rd column of the text, split the string to return the answer only
+    if(detect.LanguageDetector().detect_lang(ans, ans) != questionLang):
+        ans = translate.MSTranslator(self, None, None, "en").translate(self, ans)      #translate answer to user's desired language (i.e. language of the input question) if necesseary
+
+    return ans
+
 
 
